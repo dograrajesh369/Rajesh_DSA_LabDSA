bb93f1bc3d38d6427f9d088cf5e657ec5beaa325
Rajesh Kumar Dogra <115656754+dograrajesh369@users.noreply.github.com>
2023-01-30 23:39:20 +0530

    Add files via upload

---
+++ LAB 3-DSA/Com/gl/BST/FindSumPair.java
@@ -0,0 +1,78 @@
+package Com.gl.BST;
+
+import java.util.*;
+import java.util.Scanner;
+
+public class FindSumPair {
+
+	static class Node {
+		int nodeData;
+		Node leftNode, rightNode;
+	}
+
+	static Node NewNode(int nodeData) {
+		Node temp = new Node();
+		temp.nodeData = nodeData;
+		temp.leftNode = null;
+		temp.rightNode = null;
+
+		return temp;
+	}
+
+	public Node insert(Node root, int key) {
+		if (root == null)
+			return NewNode(key);
+		if (key < root.nodeData)
+			root.leftNode = insert(root.leftNode, key);
+		else
+			root.rightNode = insert(root.rightNode, key);
+		return root;
+	}
+
+	public boolean findpairUtil(Node root, int sum, HashSet<Integer> set) {
+		if (root == null)
+			return false;
+
+		if (findpairUtil(root.leftNode, sum, set))
+			return true;
+
+		if (set.contains(sum - root.nodeData)) {
+			System.out.println("Pair is found (" + (sum - root.nodeData) + ", " + root.nodeData + ")");
+			return true;
+//				set.add(root.nodeData);
+			// set = {10,20,30}
+			// root.nodedata = 40
+			// sum = 60
+			// set.contains(60 -40) = set.contains(20)
+		} else
+			set.add(root.nodeData);
+
+		return findpairUtil(root.rightNode, sum, set);
+	}
+
+	public void findPairWithGivenSum(Node root, int sum) {
+		HashSet<Integer> set = new HashSet<Integer>();
+		if (!findpairUtil(root, sum, set))
+			System.out.print("Pairs do not exit" + "\n");
+//			findpairUtil(root,sum,set);
+	}
+
+	// Driver code
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Node root = null;
+
+		FindSumPair findSP = new FindSumPair();
+		root = findSP.insert(root, 40);
+		root = findSP.insert(root, 20);
+		root = findSP.insert(root, 60);
+		root = findSP.insert(root, 10);
+		root = findSP.insert(root, 30);
+		root = findSP.insert(root, 50);
+		root = findSP.insert(root, 70);
+		System.out.println("Enter your Number: ");
+		int sum = sc.nextInt();
+		findSP.findPairWithGivenSum(root, sum);
+		sc.close();
+	}
+}
